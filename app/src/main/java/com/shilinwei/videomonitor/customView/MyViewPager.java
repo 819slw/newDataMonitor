2e3cda5847ace52099e73c8d85d86b2d9ed90918
shlw@toplion.com.cn <shlw@toplion.com.cn>
2022-09-26 23:45:52 +0800

    fix:重写ViewPager为MyViewPager，在滑动过渡的时候会出现多余的动画

---
+++ app/src/main/java/com/shilinwei/videomonitor/customView/MyViewPager.java
@@ -0,0 +1,23 @@
+package com.shilinwei.videomonitor.customView;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
+
+public class MyViewPager extends ViewPager {
+    public MyViewPager(@NonNull Context context) {
+        super(context);
+    }
+
+    public MyViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void setCurrentItem(int item) {
+        super.setCurrentItem(item, false);
+    }
+}
