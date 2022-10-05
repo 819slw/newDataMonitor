8bf3dbf8ac7f1a65174cbef14f32e3e550926361
shlw@toplion.com.cn <shlw@toplion.com.cn>
2022-10-05 14:45:45 +0800

    fix:新建详情界面

---
+++ app/src/main/java/com/shilinwei/videomonitor/activity/DeviceDetailActivity.java
@@ -0,0 +1,16 @@
+package com.shilinwei.videomonitor.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shilinwei.videomonitor.R;
+
+public class DeviceDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_detail);
+    }
+}
