ae702f2d0dc1f8e99f6b483ddbefb8e39e15e158
shlw@toplion.com.cn <shlw@toplion.com.cn>
2022-10-10 17:13:36 +0800

    fix:完成定位

--- app/src/main/java/com/shilinwei/videomonitor/fragment/MapFragment.java
+++ app/src/main/java/com/shilinwei/videomonitor/fragment/MapFragment.java
@@ -6,11 +6,18 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.MapsInitializer;
 import com.amap.api.maps.model.LatLng;
@@ -27,6 +34,8 @@ import com.shilinwei.videomonitor.entity.LoginResponseEntity;
 import com.shilinwei.videomonitor.entity.OutDestructionEntity;
 import com.shilinwei.videomonitor.entity.OutDestructionResponseEntity;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -39,8 +48,18 @@ public class MapFragment extends BaseFragment {
     List<OutDestructionEntity> deviceList;
     AMap aMap;
 
+    public AMapLocationClient mlocationClient;
+    //声明mLocationOption对象
+    public AMapLocationClientOption mLocationOption = null;
+
+    public double latitude;
+    public double longitude;
+
+
     public MapFragment() {
     }
+
+
 
     public static MapFragment newInstance() {
         MapFragment fragment = new MapFragment();
@@ -51,6 +70,63 @@ public class MapFragment extends BaseFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
+
+
+    public void positonLocation() {
+        try {
+            mlocationClient = new AMapLocationClient(getActivity());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //初始化定位参数
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位监听
+        mlocationClient.setLocationListener(new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(AMapLocation aMapLocation) {
+                System.out.println("进入回调");
+                if (aMapLocation != null) {
+                    if (aMapLocation.getErrorCode() == 0) {
+                        //定位成功回调信息，设置相关消息
+//                        aMapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                        //                        aMapLocation.getAccuracy();//获取精度信息
+//                        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//                        Date date = new Date(aMapLocation.getTime());
+                        //                        df.format(date);//定位时间
+                        latitude = aMapLocation.getLatitude();//获取纬度
+                        longitude = aMapLocation.getLongitude();//获取经度
+                        LatLng latLng1 = new LatLng(latitude ,longitude);
+                        aMap.addMarker(new MarkerOptions().position(latLng1).title("我在这"));
+                        aMap.moveCamera(CameraUpdateFactory.changeLatLng(new LatLng(latitude, longitude)));
+                    } else {
+                        System.out.println("获取失败2");
+                        System.out.println(aMapLocation.getErrorInfo());
+                        Log.e("AmapError","location Error, ErrCode:"
+                                + aMapLocation.getErrorCode());
+                    }
+                }else {
+                    System.out.println("获取失败1");
+                }
+            }
+        });
+        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置定位间隔,单位毫秒,默认为2000ms
+        mLocationOption.setInterval(2000);
+        //设置定位参数
+        mlocationClient.setLocationOption(mLocationOption);
+        // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+        // 注意设置合适的定位时间的间隔（最小间隔支持为1000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+        // 在定位结束后，在合适的生命周期调用onDestroy()方法
+        // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+        //启动定位
+        mlocationClient.startLocation();
+    }
+
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -68,6 +144,7 @@ public class MapFragment extends BaseFragment {
             aMap = mMapView.getMap();
         }
         initDevicceList();
+        positonLocation();
         return v;
     }
 
